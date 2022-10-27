603f0f86ccc60cea987121c6cbbafd67acbc837c
Camilo Viracachá <viracam@gmail.com>
2022-10-27 18:41:50 -0500

    con hashes, compresion y minificacion de archivos

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         assetModuleFilename: 'assets/images/[hash][ext][query]'
 
     },
@@ -40,7 +43,7 @@ module.exports = {
                 options:{
                     limit: 10000,
                     mimetype: "application/font-woff",
-                    name: "[name].[ext]",
+                    name: "[name].[contenthash].[ext]",
                     outputPath: "./assets/fonts/",
                     publicPath: "./assets/fonts/",
                     esModule: false,
@@ -55,7 +58,9 @@ module.exports = {
             filename: './index.html'
 
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
         new CopyPlugin({
             patterns:[
                 {
@@ -64,5 +69,12 @@ module.exports = {
                 }
             ]
         })
-    ]
+    ],
+    optimization : {
+        minimize: true,
+        minimizer:  [
+            new CssMinimizerPlugin(),
+            new TerserPlugin(),
+        ]
+    }
 }
