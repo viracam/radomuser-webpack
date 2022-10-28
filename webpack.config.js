5739498c7f335fe03240f08a7b3c682570b18e3d
Camilo Viracachá <viracam@gmail.com>
2022-10-28 11:47:51 -0500

    modo production limpia los archivos de modo dev

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -76,7 +76,8 @@ module.exports = {
                 }
             ]
         }),
-        new Dotenv()
+        new Dotenv(),
+        new CleanWebpackPlugin()
     ],
     optimization : {
         minimize: true,
